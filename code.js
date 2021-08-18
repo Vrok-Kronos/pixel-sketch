4639a197fac23dc88eeb5f6d8ade3662e88a5f98
Vrok-Kronos <kronos.vrok@gmail.com>
2021-08-18 09:16:55 -0500

    Initial commit w/ base structure

---
+++ code.js
@@ -0,0 +1,4 @@
+/*Load script as browser parses the html, but wait till its finished before running this file*/
+document.addEventListener("DOMContentLoaded", function() {
+ 	/*do stuff function*/
+});
